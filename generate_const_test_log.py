dc31df18343b3dbbe009829957728cb5d2d558da
Lewis-Seiden <lewy@seiden.us>
2024-07-30 09:50:59 -0700

    change data format to csv

--- generate_const_test_log.py
+++ generate_const_test_log.py
@@ -1,5 +1,7 @@
 import numpy as np
+import csv
 
 log = open('test_log.log', 'w')
+writer = csv.writer(log)
 for t in np.arange(0.0, 6.0, 0.1):
-  log.write(str(t) + ' 0.0 0.0 0.5 0.1 0.0 1.0 0.0 0.0\n')
+  writer.writerow((str(t) + ' 0.0 0.0 0.5 0.1 0.0 1.0 0.0 0.0\n').split())
